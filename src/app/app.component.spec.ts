392401a8fcc39cc2149beb1d0daa7c26952b4405
Pradeepal Ariyasena <dgpradeepal@gmail.com>
2025-01-11 19:36:46 +0530

    rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angulat-github-actions' title`, () => {
+  it(`should have the 'angular-github-actions' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angulat-github-actions');
+    expect(app.title).toEqual('angular-github-actions');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angulat-github-actions');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, angular-github-actions'
+    );
   });
 });
